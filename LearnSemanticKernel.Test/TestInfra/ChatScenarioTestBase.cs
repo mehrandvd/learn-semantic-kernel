0db1bc7e885834508f4ab78acd2d99952a7571a8
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-26 03:37:06 +0330

    Udate skUnit to 0.11 and apply required refactorings.

--- LearnSemanticKernel.Test/TestInfra/ChatScenarioTestBase.cs
+++ LearnSemanticKernel.Test/TestInfra/ChatScenarioTestBase.cs
@@ -46,18 +46,6 @@ public class ChatScenarioTestBase
     }
     protected async Task<List<ChatScenario>> LoadChatScenarioAsync(string scenario)
     {
-        var testContent = await LoadChatTestAsync(scenario);
-        var scenarios = ChatScenarioParser.Parse(testContent, "");
-        return scenarios;
-    }
-
-    private async Task<string> LoadChatTestAsync(string scenario)
-    {
-        var assembly = Assembly.GetExecutingAssembly();
-        var resourceName = $"LearnSemanticKernel.Test.{scenario}.md";
-        await using Stream stream = assembly.GetManifestResourceStream(resourceName);
-        using StreamReader reader = new StreamReader(stream);
-        var result = await reader.ReadToEndAsync();
-        return result ?? "";
+        return await ChatScenario.LoadFromResourceAsync($"LearnSemanticKernel.Test.{scenario}.md", Assembly.GetExecutingAssembly());
     }
 }
